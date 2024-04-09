b6f5ef65ea8cd74b348df4af4828100c978d7f80
Alfraganus <95579943+AllowYou@users.noreply.github.com>
2024-04-09 13:17:25 +0500

    Update templete.cpp

--- templete.cpp
+++ templete.cpp
@@ -6,24 +6,69 @@
 using namespace std;
 
 #define int long long
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define endl '\n'
 #define str string
-#define fastio ios::sync_with_stdio(0), cin.tie(0);
 #define fs first
 #define ss second
-#define endl '\n'
-#define all(x) (x).begin(), (x).end()
-#define len(x) x.size()
+#define all(a) a.begin(), a.end()
 
-#define print(a)          \
-    for (auto &x : a)     \
-        cout << x << " "; \
-    cout << endl;
+void __print(signed x) { cerr << x; }
+void __print(long long x) { cerr << x; }
+void __print(unsigned long long x) { cerr << x; }
+void __print(float x) { cerr << x; }
+void __print(double x) { cerr << x; }
+void __print(long double x) { cerr << x; }
+void __print(char x) { cerr << '\'' << x << '\''; }
+void __print(const char *x) { cerr << '\"' << x << '\"'; }
+void __print(const str &x) { cerr << '\"' << x << '\"'; }
+void __print(bool x) { cerr << (x ? "true" : "false"); }
 
-#define printmp(a)    \
-    for (auto &x : a) \
-        cout << x.fs << " " << x.ss << endl;
+template <typename T, typename V>
+void __print(const array<int, 2> &x)
+{
+    cerr << '{';
+    __print(x[0]);
+    cerr << ',';
+    __print(x[1]);
+    cerr << '}';
+}
+template <typename T, typename V>
+void __print(const pair<T, V> &x)
+{
+    cerr << '{';
+    __print(x.fs);
+    cerr << ',';
+    __print(x.ss);
+    cerr << '}';
+}
+template <typename T>
+void __print(const T &x)
+{
+    int f = 0;
+    cerr << '{';
+    for (auto &y : x)
+        cerr << (f++ ? "," : ""), __print(y);
+    cerr << "}";
+}
+void _print() { cerr << "]\n"; }
+template <typename T, typename... V>
+void _print(T t, V... v)
+{
+    __print(t);
+    if (sizeof...(v))
+        cerr << ", ";
+    _print(v...);
+}
 
-const int mod = 998244353;
+#define debug(x...)               \
+    cerr << "[" << #x << "] = ["; \
+    _print(x)
+#ifdef ONLINE_JUDGE
+#define debug(x...) 42
+#endif
+
+const int mod = 1e9 + 7;
 
 void solve(){
     
@@ -31,8 +76,9 @@ void solve(){
 
 signed main()
 {
-    fastio int t = 1;
-    cin >> t;
+    fastio;
+    int t = 1;
+    // cin >> t;
     while (t--)
     {
         solve();
